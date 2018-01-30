9c9ab752df3d2f9483d21126bf4a36344fdd84b6
bdecato <decato@usc.edu>
2018-01-30 13:20:09 -0800

    Added grid vioplot for health and culturing status

--- methylation_in_out_figures.R
+++ methylation_in_out_figures.R
@@ -11,6 +11,10 @@ if (length(args)!=1) {
 data<-read.table(args[1])
 
 pdf("meth_in_out_vioplots_allsamples.pdf")
-ggplot(data=data,aes(x=V8,y=V12,group=V8))+facet_wrap(~V1)+geom_violin(draw_quantiles=c(0.25,0.5,0.75))+xlab("Sample")+ylab("%mCpG")+theme(text = element_text(size=5), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour="black"),strip.background=element_rect(fill="white"))
+ggplot(data=data,aes(x=V8,y=V12,group=V8,fill=V11/V12))+facet_wrap(~V1)+geom_violin(draw_quantiles=c(0.25,0.5,0.75))+xlab("Sample")+ylab("%mCpG")+theme(text = element_text(size=5), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour="black"),strip.background=element_rect(fill="white"))
+dev.off()
+
+pdf("pmd_depth_by_culturing_and_health.pdf")
+ggplot(data=data,aes(x=V8,y=V12,group=V8))+facet_grid(V3~V4)+geom_violin(draw_quantiles=c(0.25,0.5,0.75))+ylab("%mCpG")+theme(text = element_text(size=10), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour="black"),strip.background=element_rect(fill="white"))
 dev.off()
 
