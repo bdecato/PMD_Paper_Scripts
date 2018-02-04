93786182e51afba7870c60759c6c72ec18d9f5f7
Benjamin Decato <decato@usc.edu>
2018-02-04 10:09:21 -0800

    Minor adjustments to make the scripts easier to use.

--- CGI_roimethstat.sh
+++ CGI_roimethstat.sh
@@ -1,13 +1,14 @@
 ## Script to calculate methylation levels inside CGIs for each PMD-containing mouse and human sample.
-## Takes two arguments: paths to the CGI annotation file for mouse and humans.
+## Takes 3 arguments: paths to the CGI annotation file for mouse and humans.
+## and prefix for path to the repo
 ## I used mm10_cpgislands.bed and hg19_cpgislands.bed, which are produced via annotations.sh from raw CGIs downloaded from the UCSC table browser.
 #!/bin/bash
 
 grep "Human" pmd_samplenames > human_pmdsamplenames
-for i in $(cat human_pmdsamplenames); do qsub -v IN=${i}.meth,REF=${1} do-roimethstat-islands.pbs; sleep 0.1; done
+for i in $(cat human_pmdsamplenames); do qsub -v IN=${i}.meth,REF=${1} $3/do-roimethstat-islands.pbs; sleep 0.1; done
 
 grep "Mouse" pmd_samplenames > mouse_pmdsamplenames
-for i in $(cat mouse_pmdsamplenames); do qsub -v IN=${i}.meth,REF=${2} do-roimethstat-islands.pbs; sleep 0.1; done
+for i in $(cat mouse_pmdsamplenames); do qsub -v IN=${i}.meth,REF=${2} $3/do-roimethstat-islands.pbs; sleep 0.1; done
 
 rm human_pmdsamplenames mouse_pmdsamplenames;
 
--- summarize_shadows.sh
+++ summarize_shadows.sh
@@ -1,4 +1,4 @@
-## 1 argument: species used (Human / Mouse)
+## 2 arguments <species used (Human / Mouse)> <output_filename>
 
 #!/bin/bash
 
@@ -6,6 +6,6 @@ for i in ${1}/*inPMDs.roimethstat; do
   printf "%s\t" $(basename ${i} .meth.inPMDs.roimethstat); 
   awk 'BEGIN{total=0;count=0}{split($4,a,":"); total+=$5*a[5]; count+=a[5]}END{print total/count}' ${i} | tr "\n" " ";
   awk 'BEGIN{total=0;count=0}{split($4,a,":"); total+=$5*a[5]; count+=a[5]}END{print total/count}' $(dirname ${i})/$(basename ${i} .inPMDs.roimethstat).outPMDs.roimethstat;
-done | awk '{print $0 "\t" $3-$2}' | sort -k 4,4g
+done | awk '{print $0 "\t" $3-$2}' | sort -k 4,4g > $2
 
 
