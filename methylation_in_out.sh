57d35fd5257f7dd625501ebafbcbb47a220484b5
Benjamin Decato <decato@usc.edu>
2018-02-04 11:08:52 -0800

    Minor changes for reproducibility

--- methylation_in_out.sh
+++ methylation_in_out.sh
@@ -9,7 +9,7 @@ for i in $(find . -name "*.roimethstat"); do
   awk -v name=${sname} -v ctxt=${context} -v spec=${species} '{split($4,a,":"); print name "\t" ctxt "\t" spec "\t" $3-$2 "\t" a[2] "\t" $5}' ${i};
 done > Out_In_PMD_Statistics
 
-awk '{if(NF==5){print $1 "\tpmd\t" $2 "\t" $3 "\t" $4 "\t" $5}else{print $0}}' Out_In_PMD_Statistics
+awk '{if(NF==5){print $1 "\tpmd\t" $2 "\t" $3 "\t" $4 "\t" $5}else{print $0}}' Out_In_PMD_Statistics > temp; mv temp Out_In_PMD_Statistics;
 sort -k 1b,1 Out_In_PMD_Statistics > Out_In_PMD_Statistics.tojoin;
 sort -k 1b,1 manifest > manifest.tojoin
 join manifest.tojoin Out_In_PMD_Statistics.tojoin > Out_In_PMD_Statistics.joined;
