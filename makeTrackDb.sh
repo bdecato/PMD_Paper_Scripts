99e30d752f1d2594ac9d314bb1957f06ded30f00
Benjamin Decato <decato@usc.edu>
2018-03-30 10:35:32 -0700

    Updates to making bigWig and bigBed tracks and building trackhub scripts

--- makeTrackDb.sh
+++ makeTrackDb.sh
@@ -1,5 +1,6 @@
-# Needs updating, but I ran this on each subdirectory of a trackhub to get its trackDb.txt file. It assumes a pmd.bb file for each parent directory that can be empty.
+# Needs updating, but I ran this on each subdirectory of a trackhub to get its trackDb.txt file. It assumes a pmd.bb file for each parent directory that can be empty and adds its corresponding methylation, coverage, HMR, boundary, and posterior tracks to the trackDb file.
 #!/bin/bash
+
 minimum_size=345;
 priority=0;
 for i in *.pmd.bb; do
@@ -10,20 +11,24 @@ for i in $(cat unique_parent_names); do
   printf "track %s\nsuperTrack on\nshortLabel %s\nlongLabel %s\npriority %s\ngroup %s\nvisibility full\n\n" ${i} ${i} ${i} ${priority} ${i};
   let priority="${priority}+1";
 
-  for j in ${i}*pmd.bb; do
+  for j in ${i}*.pmd.bb; do
     if [[ $actualsize -ge $minimumsize ]]; then
       printf "  track %s\n  parent %s\n  visibility dense\n  priority %s\n  longLabel %s\n  shortLabel %s\n  type bigBed\n  color 0,0,0\n  bigDataUrl %s\n\n" $(basename ${j} .pmd.bb) ${i} ${priority} $(basename ${j} .pmd.bb) $(basename ${j} .pmd.bb) ${j};
       let priority="${priority}+1";
     fi
 
-
     if [ -f $(basename ${j} .pmd.bb).hmr.bb ]; then
-      printf "  track %s\n  parent %s\n  visibility hide\n  priority %s\n  longLabel %s\n  shortLabel %s\n  type bigBed\n  color 0,0,0\n  bigDataUrl %s\n\n" $(basename ${j} .pmd.bb).hmr.bb ${i} ${priority}  $(basename ${j} .pmd.bb).hmr.bb $(basename ${j} .pmd.bb).hmr.bb $(basename ${j} .pmd.bb).hmr.bb;
+      printf "  track %s\n  parent %s\n  visibility dense\n  priority %s\n  longLabel %s\n  shortLabel %s\n  type bigBed\n  color 200,0,200\n  bigDataUrl %s\n\n" $(basename ${j} .pmd.bb).hmr.bb ${i} ${priority}  $(basename ${j} .pmd.bb).hmr.bb $(basename ${j} .pmd.bb).hmr.bb $(basename ${j} .pmd.bb).hmr.bb;
       let priority="${priority}+1";
     fi
 
-    if [ -f $(basename ${j} .pmd.bb).pmdmasked.hmr.bb ]; then
-      printf "  track %s\n  parent %s\n  visibility hide\n  priority %s\n  longLabel %s\n  shortLabel %s\n  type bigBed\n  color 0,0,0\n  bigDataUrl %s\n\n" $(basename ${j} .pmd.bb).pmdmasked.hmr.bb ${i} ${priority}  $(basename ${j} .pmd.bb).pmdmasked.hmr.bb $(basename ${j} .pmd.bb).pmdmasked.hmr.bb $(basename ${j} .pmd.bb).pmdmasked.hmr.bb;
+    if [ -f $(basename ${j} .pmd.bb).emissions.bw ]; then
+      printf "  track %s\n  parent %s\n  visibility hide\n  priority %s\n  longLabel %s\n  shortLabel %s\n  type bigWig\n  color 200,50,100\n  autoScale off\n  viewLimits 0:1\n  yLineOnOff on\n  gridDefault on\n  windowingFunction mean\n  maxHeightPixels 24:24:24\n  bigDataUrl %s\n\n" $(basename ${j} .pmd.bb).emissions.bw ${i} ${priority}  $(basename ${j} .pmd.bb).emissions.bw $(basename ${j} .pmd.bb).emissions.bw $(basename ${j} .pmd.bb).emissions.bw;
+      let priority="${priority}+1";
+    fi
+
+    if [ -f $(basename ${j} .pmd.bb).goodboundaries.bw ]; then
+      printf "  track %s\n  parent %s\n  visibility hide\n  priority %s\n  longLabel %s\n  shortLabel %s\n  type bigWig\n  color 50,200,50\n  autoScale off\n  viewLimits 0:1\n  yLineOnOff on\n  gridDefault on\n  windowingFunction maximum\n  maxHeightPixels 24:24:24\n  bigDataUrl %s\n\n" $(basename ${j} .pmd.bb).goodboundaries.bw ${i} ${priority}  $(basename ${j} .pmd.bb).goodboundaries.bw $(basename ${j} .pmd.bb).goodboundaries.bw $(basename ${j} .pmd.bb).goodboundaries.bw;
       let priority="${priority}+1";
     fi
 
@@ -33,7 +38,7 @@ for i in $(cat unique_parent_names); do
     fi
 
     if [ -f $(basename ${j} .pmd.bb).read.bw ]; then
-      printf "  track %s\n  parent %s\n  visibility full\n  priority %s\n  longLabel %s\n  shortLabel %s\n  type bigWig\n  color 0,0,0\n  autoScale off\n  viewLimits 0:1\n  yLineOnOff on\n  gridDefault on\n  windowingFunction mean\n  maxHeightPixels 24:24:24\n  bigDataUrl %s\n\n" $(basename ${j} .pmd.bb).read.bw ${i} ${priority}  $(basename ${j} .pmd.bb).read.bw $(basename ${j} .pmd.bb).read.bw $(basename ${j} .pmd.bb).read.bw;
+      printf "  track %s\n  parent %s\n  visibility hide\n  priority %s\n  longLabel %s\n  shortLabel %s\n  type bigWig\n  color 0,0,0\n  autoScale off\n  viewLimits 0:1\n  yLineOnOff on\n  gridDefault on\n  windowingFunction mean\n  maxHeightPixels 24:24:24\n  bigDataUrl %s\n\n" $(basename ${j} .pmd.bb).read.bw ${i} ${priority}  $(basename ${j} .pmd.bb).read.bw $(basename ${j} .pmd.bb).read.bw $(basename ${j} .pmd.bb).read.bw;
       let priority="${priority}+1";
     fi
   done
--- tracks.sh
+++ tracks.sh
@@ -5,6 +5,8 @@ then
   GENOME=$2
   METH=${NAME}.meth
   PMD=${NAME}.pmd
+  HMR=${NAME}.hmr
+  EMISSIONS=${NAME}.emissions
 
   if [ ! -e $GENOME ]
   then
@@ -23,14 +25,14 @@ then
       then
         echo "Generating $METH.bw..."
         cut -f 1-3,5 ${METH} | awk 'BEGIN{pc=0;ps=0;pe=0}{if(!($1==pc&&$2==ps&&$3==pe)){print}pc=$1;ps=$2;pe=$3}' > temp$$
-        /home/cmb-panasas2/decato/bin/bedGraphToBigWig temp$$ ${GENOME} ${METH}.bw
+        bedGraphToBigWig temp$$ ${GENOME} ${METH}.bw
       fi
 
       if [ ! -e $NAME.read.bw ]
       then
         echo "Generating $NAME.read.bw..."
         awk -F "[\t,:]+" 'BEGIN{OFS="\t"}{print $1,$2,$3,$5}' ${METH} | awk 'BEGIN{pc=0;ps=0;pe=0}{if(!($1==pc&&$2==ps&&$3==pe)){print}pc=$1;ps=$2;pe=$3}' > temp$$
-        /home/cmb-panasas2/decato/bin/bedGraphToBigWig temp$$ ${GENOME} ${NAME}.read.bw
+        bedGraphToBigWig temp$$ ${GENOME} ${NAME}.read.bw
       fi
 
     else
@@ -38,14 +40,14 @@ then
       then
         echo "Generating $METH.bw..."
         awk 'BEGIN{OFS="\t"}{print $1,$2,$2+1,$5}' ${METH} | awk 'BEGIN{pc=0;ps=0;pe=0}{if(!($1==pc&&$2==ps&&$3==pe)){print}pc=$1;ps=$2;pe=$3}' > temp$$
-        /home/cmb-panasas2/decato/bin/bedGraphToBigWig temp$$ ${GENOME} ${METH}.bw
+        bedGraphToBigWig temp$$ ${GENOME} ${METH}.bw
       fi
 
       if [ ! -e $NAME.read.bw ]
       then
         echo "Generating $NAME.read.bw..."
         awk 'BEGIN{OFS="\t"}{print $1,$2,$2+1,$6}' ${METH} | awk 'BEGIN{pc=0;ps=0;pe=0}{if(!($1==pc&&$2==ps&&$3==pe)){print}pc=$1;ps=$2;pe=$3}' > temp$$
-        /home/cmb-panasas2/decato/bin/bedGraphToBigWig temp$$ ${GENOME} ${NAME}.read.bw
+        bedGraphToBigWig temp$$ ${GENOME} ${NAME}.read.bw
       fi
     fi
   fi
@@ -55,9 +57,33 @@ then
     echo "[$PMD found.]"
     if [ ! -e $PMD.bb ]
     then
-     echo "Generating $PMD.bb..."
-     awk 'BEGIN{OFS="\t"}{print $1,$2,$3,$4,0,$6}' ${PMD} | sort -k 1,1 -k 2,2n > temp$$
+      echo "Generating $PMD.bb..."
+      awk 'BEGIN{OFS="\t"}{print $1,$2,$3,$4,0,$6}' ${PMD} | sort -k 1,1 -k 2,2n > temp$$
       bedToBigBed temp$$ ${GENOME} ${PMD}.bb
+      echo "Generating boundary tracks."
+      awk '{split($4,a,":"); if(a[3]>39){print $1 "\t" $2 "\t" $2+1 "\t" a[2]}; if(a[5]>39){print $1 "\t" $3-1 "\t" $3 "\t" a[4]};}' ${PMD} > ${PMD}.goodbounds;
+      bedGraphToBigWig ${PMD}.goodbounds ${GENOME} ${PMD}.goodbounds.bw
+    fi
+  fi
+
+  if [ ! -z $EMISSIONS ]
+  then
+    echo "[$EMISSIONS found.]"
+    if [ ! -e $EMISSIONS.bw ]
+    then
+      echo "Generating $EMISSIONS.bw..."
+      bedGraphToBigWig $EMISSIONS ${GENOME} $EMISSIONS.bw
+    fi
+  fi
+
+  if [ ! -z $HMR ]
+  then
+    echo "[$HMR found.]"
+    if [ ! -e $HMR.bb ]
+    then
+      echo "Generating $HMR.bb..."
+      awk 'BEGIN{OFS="\t"}{print $1,$2,$3,$4,0,$6}' ${HMR} | sort -k 1,1 -k 2,2n > temp$$
+      bedToBigBed temp$$ ${GENOME} ${HMR}.bb
     fi
   fi
 
