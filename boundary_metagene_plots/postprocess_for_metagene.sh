12b136f02c0d14995218b6238ec88dd76c7e0f76
Benjamin Decato <decato@usc.edu>
2018-05-04 10:07:20 -0700

    Updated metagene plot scripts to print more information (CpG density, species, cell type)

--- boundary_metagene_plots/postprocess_for_metagene.sh
+++ boundary_metagene_plots/postprocess_for_metagene.sh
@@ -1,50 +1,25 @@
 #!/bin/bash
 
-for i in $(find . -name "*.meth.roimethstat"); do awk '{split($4,a,":"); print a[3] "\t" $5}' ${i}; done > PMD_absolute_metagene
-awk '{ switch(substr($1,1,2)) {
+for i in $(find . -name "*.binnedroi"); do awk '{split($4,a,":"); split(a[1],b,"/"); print b[1] "\t" b[2] "\t" a[3] "\t" a[4] "\t" $5}' ${i}; done > PMD_absolute_metagene
+awk '{ switch(substr($3,1,2)) {
          case "lo":
-           split($1,a,"lo"); 
-           print a[2]-100 "\t" $2;
+           split($3,a,"lo"); 
+           print a[2]-100 "\t" $0;
            break
          case "li":
-           split($1,a,"li"); 
-           print a[2]-50 "\t" $2;
+           split($3,a,"li"); 
+           print a[2]-50 "\t" $0;
            break
          case "ri":
-           split($1,a,"ri"); 
-           print a[2]+50 "\t" $2;
+           split($3,a,"ri"); 
+           print a[2]+50 "\t" $0;
            break
          case "ro":
-           split($1,a,"ro"); 
-           print a[2]+100 "\t" $2;
+           split($3,a,"ro"); 
+           print a[2]+100 "\t" $0;
            break
          default:
-           print "default\t" substr($1,1,2);
+           print "default\t" substr($3,1,2);
            break
        }
 }' PMD_absolute_metagene > PMD_absolute_metagene.forR;
-
-for i in $(find . -name "*.meth.roimethstat"); do awk '{split($4,a,":"); print a[3] "\t" a[4]}' ${i}; done > PMD_absolute_metagene_density
-awk '{ switch(substr($1,1,2)) {
-         case "lo":
-           split($1,a,"lo"); 
-           print a[2]-100 "\t" $2;
-           break
-         case "li":
-           split($1,a,"li"); 
-           print a[2]-50 "\t" $2;
-           break
-         case "ri":
-           split($1,a,"ri"); 
-           print a[2]+50 "\t" $2;
-           break
-         case "ro":
-           split($1,a,"ro"); 
-           print a[2]+100 "\t" $2;
-           break
-         default:
-           print "default\t" substr($1,1,2);
-           break
-       }
-}' PMD_absolute_metagene_density > PMD_absolute_metagene_density.forR;
-
