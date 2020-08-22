eda66fe0545255afbd6bbea614579caf6a5f39b3
Ben Decato <bdecato@gmail.com>
2020-08-22 17:59:42 -0400

    Change to geom_hex for reviewer

--- F3D_PMD_depth_by_conservation.R
+++ F3D_PMD_depth_by_conservation.R
@@ -10,12 +10,12 @@ model <- lm(data=data, meanMeth~numSamples)
 summary(model)
 
 # Figure 3D.
-smoothScatter(data$numSamples,data$meanMeth)
+#smoothScatter(data$numSamples,data$meanMeth)
 
-#ggplot(data,aes(x=numSamples,y=meanMeth)) + 
-#  geom_hex() + 
-#  stat_smooth(method=lm) +
-#  theme_bw() +
-#  theme(legend.position = "right", text = element_text(size=14), axis.text = element_text(size = 10),
-#        axis.text.x = element_text(angle = 45, hjust = 1), strip.background = element_blank(),
-#        strip.placement = "outside")
+ggplot(data,aes(x=numSamples,y=meanMeth)) + 
+  geom_hex() + 
+  stat_smooth(method=lm, color="red") +
+  theme_bw() +
+  theme(legend.position = "bottom", text = element_text(size=14), axis.text = element_text(size = 10),
+        axis.text.x = element_text(angle = 45, hjust = 1), strip.background = element_blank(),
+        strip.placement = "outside")
