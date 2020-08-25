260185a359df891f0a32380d940599030ae3c7c6
Ben Decato <bdecato@gmail.com>
2020-08-24 20:38:56 -0400

    Reproduce F5A and put together reviewer requested Supp Table S4

--- F5_species_analysis.R
+++ F5_species_analysis.R
@@ -1,6 +1,30 @@
 #!/usr/bin/Rscript
 library(tidyverse)
 library(hexbin)
+library(gridExtra)
+
+### F5A
+props <- read.table("~/Desktop/Decato-PMD-revision-analysis/joined_block_prop_pmd_size_human_mouse", header=TRUE)
+human <- read.table("~/Desktop/Decato-PMD-revision-analysis/hg19_blocks.bed", header=TRUE)
+mouse <- read.table("~/Desktop/Decato-PMD-revision-analysis/mm10_blocks.bed",header=TRUE)
+
+ggplot(props,aes(x=FracCoveredHuman,y=FracCoveredMouse,color=log(BlockSizeHuman))) + 
+  geom_point() +
+  geom_smooth(method="lm") + 
+  theme_bw() +
+  theme(legend.position = "right", text = element_text(size=14), axis.text = element_text(size = 10),
+        axis.text.x = element_text(angle = 45, hjust = 1), strip.background = element_blank(),
+        strip.placement = "outside")
+
+suppTable <- left_join(props,human)
+suppTable <- left_join(suppTable, mouse)
+
+# If there's more than 70% difference in PMD coverage between human and mouse, call it a discordant block
+suppTable <- suppTable %>%
+  mutate(Discordant = ifelse(abs(FracCoveredMouse-FracCoveredHuman)>=0.7,"Yes","No"))
+
+write.table(suppTable, "~/Desktop/Decato-PMD-revision-analysis/SuppTableS5.tsv", append=FALSE, 
+            quote=FALSE, sep = "\t", row.names = FALSE)
 
 ### F5B
 data <- read.table("~/Desktop/Decato-PMD-revision-analysis/F5B_mean_TPM_long.txt",header=TRUE)
